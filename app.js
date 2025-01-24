d8f1e6a69ee4d3f8b68b4f435c2cc42cbf429611
Hoang Anh <hoanganhik172510@gmail.com>
2025-01-24 14:20:46 +0700

    start project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const {default:helmet} = require('helmet');
+const compression = require('compression');
+const app = express();
+
+// init middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+
+// init db
+// init routes
+// init error handling
+module.exports = app;
