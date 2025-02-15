82574c8526db25e11b918d779d7e59a75f22d65a
Hoang Anh <hoanganhik172510@gmail.com>
2025-02-15 22:44:18 +0700

    init inventory

--- models/repositories/inventory.repo.js
+++ models/repositories/inventory.repo.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const inventory = require('../../models/invention.model')
+const inventory = require('../../models/inventory.model')
 
 
-const createInventory = async (product_id, stock, location, shop_id)=>{
-    return await inventory.create({invent_productId:product_id, invent_stock:stock, invent_location:location, invent_shopId:shop_id})
+const createInventory = async ({productId, stock, location = 'unknow', shopId})=>{
+    return await inventory.create({invent_productId:productId, invent_stock:stock, invent_location:location, invent_shopId:shopId})
 }
 
 module.exports = {
